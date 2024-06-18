f2469a25d826bec3fc33df2c2bcd1e45da23fd60
Rolando Tarqui Benito <rolando.tarqui@etermax.com>
2024-06-17 22:48:21 -0300

    evidencia

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,36 +1,28 @@
-import fs from 'fs';
-import path from 'path';
-import { test, expect } from '@playwright/test';
-import { parse } from 'csv-parse/sync';
+import fs from "fs";
+import path from "path";
+import { test, expect } from "@playwright/test";
+import { parse } from "csv-parse/sync";
+import { format } from 'date-fns';
 
-const id_text = parse(fs.readFileSync(path.join(__dirname, 'data-test.csv')), {
+
+const id_text = parse(fs.readFileSync(path.join(__dirname, "data-test.csv")), {
   columns: true,
-  skip_empty_lines: true
+  skip_empty_lines: true,
 });
 
-
-console.log(id_text[0].message); //muestra el mensaje del id_text 0. pero en realidad es el indice, fila 0
-console.log(id_text[2].message);
-console.log(id_text[3].message)
-
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  // Build the regular expression from id_text[3].message
-  //const expectedTitleRegex = new RegExp(id_text[0].message);
-
-  // Use the regular expression with toHaveTitle
+test("saludo", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Cliente fallece", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await page.getByRole("link", { name: id_text[1].message }).click();
+  await expect(page.getByRole("heading", { name: id_text[3].message })).toBeVisible();
+  // Create a unique filename with a timestamp
+  const timestamp = Date.now();
+  const formattedDate = format(timestamp, 'yyyy-MM-dd_HH-mm-ss'); // Format the date using date-fns
 
-  // Click the get started link.
-  await page.getByRole('link', { name: (id_text[1].message) }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: (id_text[3].message) })).toBeVisible();
+  // Save the screenshot with the unique filename
+  await page.screenshot({ path: `evidencias/saludo_test_${formattedDate}.png`, fullPage: true });
 });
